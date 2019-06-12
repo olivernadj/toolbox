8c70cbfbe90a84f949145b0393e40620f2803d44
oliver nadj <mr.oliver.nadj@gmail.com>
2019-06-12 22:32:22 +0200

    Added python lessions

---
+++ python-bootcamp/two.py
@@ -0,0 +1,13 @@
+import one
+
+
+print('TOP LEVER IN TWO.PY')
+
+
+one.func()
+
+
+if __name__ == '__main__':
+    print('TWO.PY is being run directly!')
+else:
+    print('TOW.PY has been imported!')
