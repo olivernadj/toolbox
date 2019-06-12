04b158e4b148d3ce13f78963733675e64126469f
oliver nadj <mr.oliver.nadj@gmail.com>
2019-06-12 22:32:22 +0200

    Added python lessions

---
+++ python-bootcamp/myprogram.py
@@ -0,0 +1,9 @@
+# from mymodule import my_func
+#
+# my_func()
+
+from MyMainPackage import some_main_script
+from MyMainPackage.SubPackage import mysubscript
+
+some_main_script.report_main()
+mysubscript.sub_report()
---
+++ python-bootcamp/simple1.py
@@ -0,0 +1,6 @@
+a = 1
+b = 2
+
+print(a)
+
+print(B)
